5add3261b92a20b1169eebc6f16de1064f1dbaba
Miles Z. Sterrett <miles@mileszs.com>
2015-07-03 15:34:33 -0400

    Use heroku's dynamic port

--- server.js
+++ server.js
@@ -1,3 +1,3 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
-connect().use(serveStatic(__dirname)).listen(8080);
+connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8080);
