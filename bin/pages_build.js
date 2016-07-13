96bb615ed3d9d9986f5bd8055b2b84e89f23944b
Miles Z. Sterrett <miles@mileszs.com>
2016-07-13 16:04:32 -0400

    Add proper output in gh-pages build script

--- bin/pages_build.js
+++ bin/pages_build.js
@@ -1,4 +1,10 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, 'build'), function(err) { console.log('Built, yo') });
+ghpages.publish(path.join(__dirname, '..', 'build'), function(err) { 
+  if (err) {
+    console.log('Build errors OH NOES', err);
+  } else {
+    console.log('Built, yo');
+  }
+});
