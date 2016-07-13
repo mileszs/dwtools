71ac0c317a988cb3b6f1607b567e25e1207a1c3b
Miles Z. Sterrett <miles@mileszs.com>
2016-07-13 16:24:30 -0400

    Enhancing the publishing script

--- bin/pages_build.js
+++ bin/pages_build.js
@@ -3,14 +3,14 @@ var path = require('path');
 
 var logger = function(message) {
   console.log(message);
-}
+};
 
 var callback = function(err) {
   if (err) {
     console.log('Build errors OH NOES', err);
   } else {
-    console.log('Built, yo');
+    console.log("\nFinished. Published. Boom.");
   }
-}
+};
 
 ghpages.publish(path.join(__dirname, '..', 'build'), { add: true, logger: logger }, callback);
