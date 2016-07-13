bb7a8c17fba0374c6e2e613e7ca03fe3495553d9
Miles Z. Sterrett <miles@mileszs.com>
2016-07-13 16:19:23 -0400

    Preserve files on publishing

--- bin/pages_build.js
+++ bin/pages_build.js
@@ -1,10 +1,16 @@
 var ghpages = require('gh-pages');
 var path = require('path');
 
-ghpages.publish(path.join(__dirname, '..', 'build'), function(err) { 
+var logger = function(message) {
+  console.log(message);
+}
+
+var callback = function(err) {
   if (err) {
     console.log('Build errors OH NOES', err);
   } else {
     console.log('Built, yo');
   }
-});
+}
+
+ghpages.publish(path.join(__dirname, '..', 'build'), { add: true, logger: logger }, callback);
